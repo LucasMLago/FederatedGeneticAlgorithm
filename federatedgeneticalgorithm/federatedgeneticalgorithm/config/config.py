3a96e7cfd12dc7c9cb1e91923169c36d22a41bac
Lucas Martins Lago <lucas.lago@ibm.com>
2026-01-09 10:18:03 -0300

    refactor: remove unnecessary global parameters

--- federatedgeneticalgorithm/federatedgeneticalgorithm/config/config.py
+++ federatedgeneticalgorithm/federatedgeneticalgorithm/config/config.py
@@ -1,5 +1,4 @@
 import torch
-import multiprocessing
 
 # Pytorch config
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -7,16 +6,12 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Seed config
 SEED = 0
 
-# Model config
-EPOCHS = 10
-
 # Genetic Algorithm config
 CROSSOVER_PROB = 0.7
 MUTATION_PROB = 0.2
 TOURNAMENT_SIZE = 3
 POPULATION_SIZE = 10
 NUMBER_OF_GENERATIONS = 5
-NUM_WORKERS = multiprocessing.cpu_count() // 2
 
 # Multi-fidelity & Proximal Config
 RUNG0_EPOCHS = 2
