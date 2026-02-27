09251ede4fa66dff90b6a9e80f6128494bf5f0a2
Lucas <lucas.lago.terceiros@claro.com.br>
2026-02-27 09:56:58 -0300

    fix: change CNN comments

--- federatedgeneticalgorithm/federatedgeneticalgorithm/task.py
+++ federatedgeneticalgorithm/federatedgeneticalgorithm/task.py
@@ -17,7 +17,7 @@ class CNN(nn.Module):
 
     def __init__(self):
         super(CNN, self).__init__()
-        # Convolutional Block 1
+        # Convolutional layers
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
@@ -25,7 +25,6 @@ class CNN(nn.Module):
         self.pool1 = nn.MaxPool2d(2, 2)
         self.drop1 = nn.Dropout(0.2)
 
-        # Convolutional Block 2
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(64)
         self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
@@ -33,7 +32,6 @@ class CNN(nn.Module):
         self.pool2 = nn.MaxPool2d(2, 2)
         self.drop2 = nn.Dropout(0.3)
 
-        # Convolutional Block 3
         self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.bn5 = nn.BatchNorm2d(128)
         self.conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
@@ -48,28 +46,25 @@ class CNN(nn.Module):
         self.fc2 = nn.Linear(512, 10)
 
     def forward(self, x):
-        # Block 1
+        #Convolutional layers
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.pool1(x)
         x = self.drop1(x)
 
-        # Block 2
         x = F.relu(self.bn3(self.conv3(x)))
         x = F.relu(self.bn4(self.conv4(x)))
         x = self.pool2(x)
         x = self.drop2(x)
 
-        # Block 3
         x = F.relu(self.bn5(self.conv5(x)))
         x = F.relu(self.bn6(self.conv6(x)))
         x = self.pool3(x)
         x = self.drop3(x)
 
-        # Flatten
         x = torch.flatten(x, 1)
         
-        # FC Layers
+        # Fully Connected Layers
         x = F.relu(self.bn7(self.fc1(x)))
         x = self.drop4(x)
         x = self.fc2(x)
